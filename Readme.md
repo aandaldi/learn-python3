38c2086d97b5e8a7a23c5c56d21a69b570b91ce7
aandaldi <aandaldi@gmail.com>
2019-06-25 01:12:50 +0700

    Data Type and and condition, Loops, and Exceptions (test1.py)

--- Readme.md
+++ Readme.md
@@ -1,1 +1,18 @@
-readme learn-python3
+Learn Python:
+1. show output
+2. Basic User Input
+3. Variables
+4. Data Types
+    
+    - Strings  => indexing, methodes, operators & formatting, slices,
+    - Numbers  => math operator, logical Operators 
+    - Booleans
+    - Lists    => Methodes, Slices
+    - Sets     => Methodes, Frozensets
+    - Tuples   => Methodes, 
+    - Ranges   => Methodes, 
+    - Dictionaries => Methodes, convert beetwen Data Types
+    - None
+5. Conditionals 
+6. Loops
+7. Exceptions
---
+++ test1.py
@@ -0,0 +1,73 @@
+# Conditionals
+#######IF ELIF ELSE (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+
+# x = 10
+# y = 4                                                            # you can change variabel on if
+# z = 2                                                            # you can change variabel on if
+# if x < 5 or type(x) is int:
+#     print("x is greater than 5")
+#     print(x*2)
+# elif  y==4:
+#     print("y is 4")
+# else:
+#     print("x is not greater than 5")
+
+
+####### FOR-ELSE (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+
+# vendors = ["Cisco","HP", "Nortel", "Avaya", "Juniper"]
+# for each_vendor in vendors:                                        #this statement will be print every index on vendors
+#     print(each_vendor)
+#
+# for index, element in enumerate(vendors):                          #enumerate function for python returns both the index and the element value at the same time
+#     print(index, element)
+# else:
+#     print("\nThe end of the list has been reached")                  #else will be run if the for has been finish
+
+
+####### LOOPS - WHILE-ELSE (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+# x = 1
+# y=100                                                               # you can change variabel on if
+# while y <= 10 :
+#     print(x)
+#     x = x +1
+# else:
+#     print (y)
+
+
+####### NESTING - IF FOR WHILE (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+
+# x = "Cisco"
+#
+# if "i" in x:
+#     if len(x) > 3:
+#         print(x, len(x))
+#
+# list1 = [4, 5, 6]
+# list2 = [10,20, 30]
+#
+# for i in list1 :                                                    # this will be print from index 0 in list1
+#     for j in list2 :
+#         print(i * j)
+#     print(i)
+
+
+
+####### BREAK - CONTINUE - PASS (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+#
+# for number in range(10):
+#     if number == 7 :
+#         break
+#     print(number)
+
+
+
+####### TRY EXCEPT ELSE FINALLY (REMOVE THE COMMAND TO RUN THIS BLOCK PROGRAM)
+
+for i in range(5) :
+    try:
+        print(i / 1)
+    except ZeroDivisionError as e:
+        print(e, "--> Division by 0 is not allowed, sorryy!")
+    finally:
+        print("I don't care, I'm getting print either way")
